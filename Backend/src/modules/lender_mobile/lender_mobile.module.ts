6544c1e04c75000932dbf047ec8d780d9c1a42c6
kaushani rasadika <rasadikak@gmail.com>
2026-02-18 12:47:39 +0530

    added lender/borrower api end points

--- Backend/src/modules/lender_mobile/lender_mobile.module.ts
+++ Backend/src/modules/lender_mobile/lender_mobile.module.ts
@@ -2,16 +2,20 @@ import { Module, Logger } from '@nestjs/common';
 
 import { LenderMobileController } from './lender_mobile.controller';
 import { LenderMobileService } from './lender_mobile.service';
+
 import { LenderApplicationsController } from './lender_applications.controller';
 import { LenderApplicationsService } from './lender_applications.service';
+import { LenderBorrowersController } from './lender_borrowers.controller';
+import { LenderBorrowersService } from './lender_borrowers.service';
 
 @Module({
-  controllers: [LenderMobileController, LenderApplicationsController],
+  controllers: [LenderMobileController, LenderApplicationsController, LenderBorrowersController],
   providers: [
     LenderMobileService,
     LenderApplicationsService,
+    LenderBorrowersService,
     Logger,
   ],
-  exports: [LenderMobileService, LenderApplicationsService], // allows reuse in other modules if needed
+  exports: [LenderMobileService, LenderApplicationsService, LenderBorrowersService], // allows reuse in other modules if needed
 })
 export class LenderMobileModule {}
